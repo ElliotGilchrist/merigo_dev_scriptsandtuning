07576ece2e24c7d750d4f2935fa4c3f4512b5fab
Elliot Gilchrist <elliot@elliotgilchrist.com>
2022-04-04 14:11:51 -0700

    area crash wip

--- area_crash/scripts/batch.exs
+++ area_crash/scripts/batch.exs
@@ -1,18 +1,22 @@
 defmodule PGS.Batch do
     @behaviour Sys.Script.Batch
 
+    @batch_count 20
+    @action_count 500
+
     def bulk_test() do
-        Enum.each(1..20, fn _ -> Sys.Batch.run("bulk_test") end)
+        Enum.each(1..@batch_count, fn i -> Sys.Batch.run("bulk_test", i) end)
     end
 
     @impl true
-    def onRun("bulk_test", _args, _id, _status) do
-        Sys.Log.debug("Starting bulk_test batch")
-        for _ <- 1..500 do
+    def onRun("bulk_test", batch_index, _id, _status) do
+        Sys.Log.info("Starting bulk_test batch #{batch_index}")
+        for i <- 1..@action_count do
+            Sys.Log.info("[Batch #{batch_index} - Action #{i}]")
             name = PGS.AreaManager.random_area()
             PGS.AreaManager.random_action(name)
         end
-        Sys.Log.debug("Finished bulk_test batch")
+        Sys.Log.info("Finished bulk_test batch #{batch_index}")
         :ok
     end
 
