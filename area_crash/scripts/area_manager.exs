6d86806aa866dac68d3ea803922e6a696acaf97c
Elliot Gilchrist <elliot@elliotgilchrist.com>
2022-04-04 14:08:22 -0700

    area crash wip

--- area_crash/scripts/area_manager.exs
+++ area_crash/scripts/area_manager.exs
@@ -7,32 +7,28 @@ defmodule PGS.AreaManager do
     end
 
     def create_area_sync(name) do
+        Sys.Log.info("Creating #{name} sync")
         Sys.Area.Grid.Square.create(name, "default", 1, 16, 16, false, :perpetual, :persistent, nil)
     end
 
     def create_area_async(name) do
+        Sys.Log.info("Creating #{name} async")
         Sys.Area.Grid.Square.createAsync(name, "default", 1, 16, 16, false, :perpetual, :persistent, nil)
-    end
-
-    def info() do
-        msg = "All areas: #{inspect Sys.Area.all()}"
-        Sys.Log.info(msg)
-        msg
     end
 
     def random_action(name) do
         case Sys.Random.integer(5) do
             0 -> create_area_async(name)
             1 -> create_area_sync(name)
-            2 -> Sys.Area.destroy(name)
-            3 -> Sys.Area.command(name, "populate", nil)
-            4 -> Sys.Area.notify(name, "populate", nil)
+            2 ->
+                Sys.Log.info("Destroying #{name}")
+                Sys.Area.destroy(name)
+            3 ->
+                Sys.Log.info("Populating (sync) #{name}")
+                Sys.Area.command(name, "populate", nil)
+            4 ->
+                Sys.Log.info("Populating (async) #{name}")
+                Sys.Area.notify(name, "populate", nil)
         end
-    end
-
-    def populate(name) do
-        Sys.Area.setLocalProperty("foo", "bar")
-        Sys.Area.setSharedProperty("beep", "boop")
-        Sys.Area.add(0, {2,2}, Sys.Utils.createKey, :element, nil)
     end
 end
--- area_crash/scripts/batch.exs
+++ area_crash/scripts/batch.exs
@@ -2,15 +2,17 @@ defmodule PGS.Batch do
     @behaviour Sys.Script.Batch
 
     def bulk_test() do
-        Enum.each(1..20, fn _ -> Sys.Batch.run("cycle_area") end)
+        Enum.each(1..20, fn _ -> Sys.Batch.run("bulk_test") end)
     end
 
     @impl true
-    def onRun("cycle_area", _args, _id, _status) do
+    def onRun("bulk_test", _args, _id, _status) do
+        Sys.Log.debug("Starting bulk_test batch")
         for _ <- 1..500 do
             name = PGS.AreaManager.random_area()
             PGS.AreaManager.random_action(name)
         end
+        Sys.Log.debug("Finished bulk_test batch")
         :ok
     end
 
