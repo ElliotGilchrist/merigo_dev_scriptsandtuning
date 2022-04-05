ff642c4cbc443bbca000a54419063b3db7cab787
Elliot Gilchrist <elliot@elliotgilchrist.com>
2022-04-05 10:28:17 -0700

    area crash wip

--- area_crash/scripts/area.exs
+++ area_crash/scripts/area.exs
@@ -2,12 +2,34 @@ defmodule PGS.Area do
     @behaviour Sys.Script.Area
 
     @impl true
+    def onCreate(_args) do
+        Sys.Log.info("[PGS.Area #{Sys.Area.name}] onCreate")
+    end
+
+    @impl true
+    def onDestroy(_args) do
+        Sys.Log.info("[PGS.Area #{Sys.Area.name}] onDestroy")
+    end
+
+    @impl true
+    def onStart(_args) do
+        Sys.Log.info("[PGS.Area #{Sys.Area.name}] onStart")
+    end
+
+    @impl true
+    def onStop(_args) do
+        Sys.Log.info("[PGS.Area #{Sys.Area.name}] onStop")
+    end
+
+    @impl true
     def doCommand("populate", _) do
+        Sys.Log.info("[PGS.Area #{Sys.Area.name}] Populating (sync)")
         populate()
     end
 
     @impl true
     def onNotify("populate", _) do
+        Sys.Log.info("[PGS.Area #{Sys.Area.name}] Populating (async)")
         populate()
     end
 
@@ -15,5 +37,6 @@ defmodule PGS.Area do
         Sys.Area.setLocalProperty("foo", "bar")
         Sys.Area.setSharedProperty("beep", "boop")
         Sys.Area.add(0, {2,2}, Sys.Utils.createKey, :element, nil)
+        Sys.Log.info("[PGS.Area #{Sys.Area.name}] Finished populating")
     end
 end
--- area_crash/scripts/game.exs
+++ area_crash/scripts/game.exs
@@ -5,6 +5,26 @@ defmodule PGS.Game do
     def doPortal("bulk_test", _) do
         PGS.Batch.bulk_test()
     end
+
+    def doPortal("create_async", %{"name" => name}) do
+        PGS.AreaManager.create_area_async(name)
+    end
+
+    def doPortal("create_sync", %{"name" => name}) do
+        PGS.AreaManager.create_area_sync(name)
+    end
+
+    def doPortal("destroy", %{"name" => name}) do
+        Sys.Area.destroy(name)
+    end
+
+    def doPortal("populate_async", %{"name" => name}) do
+        Sys.Area.notify(name, "populate", nil)
+    end
+
+    def doPortal("populate_sync", %{"name" => name}) do
+        Sys.Area.command(name, "populate", nil)
+    end
 end
 
 
