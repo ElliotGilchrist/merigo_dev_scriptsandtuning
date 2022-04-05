591a1a9feb08c974b6f0f78751c01de7f04004fe
Elliot Gilchrist <elliot@elliotgilchrist.com>
2022-04-05 10:49:48 -0700

    area crash wip

--- area_crash/scripts/area_manager.exs
+++ area_crash/scripts/area_manager.exs
@@ -7,39 +7,44 @@ defmodule PGS.AreaManager do
     end
 
     def create_area_sync(name) do
+        Sys.Log.info("[#{name}] Creating (sync) ->")
         Sys.Area.Grid.Square.create(name, "default", 1, 16, 16, false, :perpetual, :persistent, nil)
+        Sys.Log.info("[#{name}] <- Created (sync)")
     end
 
     def create_area_async(name) do
+        Sys.Log.info("[#{name}] Creating (async)")
         Sys.Area.Grid.Square.createAsync(name, "default", 1, 16, 16, false, :perpetual, :persistent, nil)
     end
+
+    def populate_sync(name) do
+        Sys.Log.info("[#{name}] Populating (sync) ->")
+        Sys.Area.command(name, "populate", nil)
+        Sys.Log.info("[#{name}] <- Populated (sync)")
+    end
+
+    def populate_async(name) do
+        Sys.Log.info("[#{name}] Populating (async)")
+        Sys.Area.notify(name, "populate", nil)
+    end
+
+    def destroy(name) do
+        Sys.Log.info("[#{name}] Destroying")
+        Sys.Area.destroy(name)
+    end
+
+    @actions [:create_area_sync, :create_area_async, :populate_sync, :populate_async, :destroy]
 
     def random_action(info) do
         name = PGS.AreaManager.random_area()
         info = info <> " " <> name
         try do
-            case Sys.Random.integer(5) do
-                0 ->
-                    Sys.Log.info("#{info} Creating (async)")
-                    create_area_async(name)
-                1 ->
-                    Sys.Log.info("#{info} Creating (sync)->")
-                    create_area_sync(name)
-                    Sys.Log.info("#{info} <-Created (sync)")
-                2 ->
-                    Sys.Log.info("#{info} Destroying")
-                    Sys.Area.destroy(name)
-                3 ->
-                    Sys.Log.info("#{info} Populating (sync)->")
-                    Sys.Area.command(name, "populate", nil)
-                    Sys.Log.info("#{info} <-Populating (sync)")
-                4 ->
-                    Sys.Log.info("#{info} Populating (async)")
-                    Sys.Area.notify(name, "populate", nil)
-            end
+            action = Sys.Random.sample(@actions)
+            Sys.Log.info(info <> " #{action}")
+            apply(__MODULE__, action, [name])
         rescue
             e ->
-                Sys.Log.info("Rescued, #{inspect e}")
+                Sys.Log.error("Rescued, #{inspect e}")
         end
     end
     :ok
